5957f2aa9623ec5e7c55d4623c1ec221fb787a96
Abhishek Bongale <abhishekbongale@outlook.com>
2026-05-03 12:53:52 +0100

    Fix GitHub Pages deployment by removing conflicting config file
    The presence of next.config.js was preventing next.config.ts from being
    used, which has the proper static export configuration (output: 'export')
    needed for GitHub Pages. This was causing builds to create .next directory
    instead of the required out directory.
    
    With this fix:
    - GitHub Pages deployment will now succeed with static export
    - Images will display correctly with proper basePath configuration
    - All static assets will be properly optimized and copied
    
    Signed-off-by: Abhishek Bongale <abhishekbongale@outlook.com>

--- next.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  assetPrefix: process.env.GITHUB_PAGES ? '/' + process.env.PUBLISH_DIR + '/' : '',
-  basePath: process.env.GITHUB_PAGES ? '/' + process.env.PUBLISH_DIR : '',
-  trailingSlash: true,
-  reactStrictMode: true,
-};
